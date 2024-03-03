0d9c5e80b12a2eff6e2395d2408267955787ace1
AnikaPuri <42912590+AnikaPuri@users.noreply.github.com>
2024-03-02 20:14:22 -0500

    push

--- global.js
+++ global.js
@@ -3,3 +3,38 @@ console.log("IT’S ALIVE!");
 function $$ (selector, context = document) {
 	return Array.from(context.querySelectorAll(selector));
 }
+
+//const navLinks = $$("nav a");
+
+//let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
+
+/*
+currentLink.classList.add("current");
+
+if (currentLink) { // or if (currentLink !== undefined)
+	currentLink.classList.add("current");
+}
+*/
+
+//currentLink?.classList.add("current");
+
+let pages = [
+	{url: "", title: "Home"},
+	{url: "projects/", title: "Projects"},
+    {url: "contact/", title: "Contact"},
+    {url: "resume/", title: "Resume"},
+    {url: "https://github.com/AnikaPuri", title: "Profile"},
+	
+];
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+	let url = p.url;
+	let title = p.title;
+    
+	// Create link and add it to nav
+    nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>` );
+
+}
